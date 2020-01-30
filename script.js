7af8d3a676493a449bb92368d9efe649b3654e4b
Kevin_Isaza <2089276@talnet.nl>
2020-01-30 10:09:49 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,26 @@
+const antwoorden = {
+  Frankrijk: "Parijs",
+  spin: "8",
+  meer: "IJsselmeer",
+  auto: "Audi",
+  waad: "Texel"
+};
+
+function checkAnwsers() {
+  let allGood = true;
+
+  Object.keys(antwoorden).forEach(function(item) {
+    let element = document.getElementById(item);
+    if (element.value === antwoorden[item]) {
+      element.classList.add("bg-green");
+    } else {
+      element.classList.add("bg-red");
+      allGood = false;
+    }
+  });
+
+  if (allGood) {
+    document.getElementById("correct").innerText =
+      "Alle antwoorden zijn correct";
+  }
+}
